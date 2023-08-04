75386367fd66ff0f1ed0ea87c2de0206a5d59d94
jun <znznun@gmail.com>
2023-08-04 16:16:43 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState([]);
+  const [inputVal, setInputVal] = useState('');
+  const inputRef = useRef();
+  const [modeCount, setModeCount] = useState(true);
+
+  useEffect(() => {
+    if (modeCount) {
+      document.body.style.background = 'white';
+    } else {
+      document.body.style.background = 'black';
+    }
+  }, [modeCount])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className={modeCount === true ? 'bg' : 'bg darkmode darkBg'}>
+        <h2 className={modeCount === true ? 'title' : 'title darkmode'}>ToDoList</h2>
+        <h5 className={modeCount === true ? 'sub-title' : 'sub-title darkmode'}>Organize your plans for today!</h5>
+        <div className='form'>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            let inputValue = inputRef.current.value.trim();
+            if(inputValue !== '') {
+              let copyText = [...text];
+              copyText.push(inputValue)
+              copyText = new Set(copyText);
+              copyText = Array.from(copyText);
+              setText(copyText);
+              setInputVal('');
+            }
+          }}>
+            <input ref={inputRef} value={inputVal} onChange={(e) => { setInputVal(e.target.value) }} className={modeCount === true ? null : 'darkmode'} />
+            <button className={modeCount === true ? 'btn' : 'btn darkmode darkbtnbackground'}>result</button>
+          </form>
+        </div>
+        {
+          text.map((d, i) => {
+            return <TextBox d={d} i={i} text={text} setText={setText} key={i} darkmode={modeCount === true ? '' : 'darkmode darkcontentbackground'}  
+            darkbtn={modeCount === true ? '' : 'darkmode darkcontentbackground darkbtn'}/>
+          })
+        }
+        <div className='modeBtn'>
+          <button className={modeCount === true ? 'btn mar' : 'btn darkmode darkbtnbackground mar'}
+            onClick={() => { setModeCount(!modeCount) }}>
+            {modeCount === true ? <span>darkmode</span> : <span>lightmode</span>}
+          </button>
+        </div>
+      </div>
+
     </div>
   );
+}
+
+function TextBox({ d, i, text, setText, darkmode,darkbtn }) {
+  return (
+    <div className={`textBg ${darkmode} contentColor`}>
+      <button onClick={() => {
+        let updateText = [...text];
+        updateText.splice(i, 1);
+        setText(updateText);
+      }} className={`remove btn ${darkmode} ${darkbtn}`}>X</button>
+      <span className={`content ${darkmode}`}>{i + 1}. {d}</span>
+    </div>
+  )
 }
 
 export default App;
